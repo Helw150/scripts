e3ff357c54eb3356ec0821d3840b3777c6a1ec38
wbh230@nyu.edu <wbh230@nyu.edu>
2016-09-28 23:30:25 +0400

    Simple script to close and open the Cost function on tensorboard for easier viewing on big screen

---
+++ AutoUpdateTensorBoard.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+    var elements = document.getElementsByClassName("heading style-scope tf-collapsable-pane");
+    var requiredElement = elements[0];
+    requiredElement.click();
+    requiredElement.click();
+}, 5000);
